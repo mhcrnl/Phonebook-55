f49a9daf066a5d0d3024e95904f41abe7a56d3e6
Polina Mansurova <polinkapolinka1@gmail.com>
2018-05-04 11:26:21 +0300

    addid issuis to project

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""issuis
+1.Add record
+  1.1.input name and phonenumber
+  1.2 save name and phonenumber in the storage
+2. Show all records
+  2.1 get all records of the storage
+  2.2 display records
+"""
 print ("MyPhonebook")
 
 
