42df3269ed135bc95283e463272a77762314463d
Igor Mansurov <igorm@crow.co.il>
2018-05-04 11:47:34 +0300

    fixed some spelling in issues

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """
 issues
 1.Add record
-  1.1.input name and phonenumber
-  1.2 save name and phonenumber in the storage
+  1.1.input name and phone number
+  1.2 save name and phone number in the storage
 2. Show all records
   2.1 get all records of the storage
   2.2 display records
