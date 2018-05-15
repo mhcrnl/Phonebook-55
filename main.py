6dbb5cb3c50b15a03b69334662cc50cf3efd369a
Polina Mansurova <polinkapolinka1@gmail.com>
2018-05-15 12:11:20 +0300

    added function data deletion

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ issues
 """
 base=[]
 def start():
-    a = '4'
-    while a != '3':
+    a = '5'
+    while a != '4':
      print('What do you want to do?')
      print('1.Enter a new record')
      print('2.Output all records')
-     print('3.Quit the program')
+     print('3. Delete abonent')
+     print('4.Quit the program')
      a=input()
      if a=='1':
          rec = inpute_name_and_phone_number()
          save_name_and_phone_number_in_the_storage(rec)
+
      if a=='2':
        print(base)
      if a=='3':
+         del_abon()
+         print(base)
+     if a=='4':
        exit()
 def inpute_name_and_phone_number():
     name = input('Enter name: ')
@@ -29,34 +34,39 @@ def inpute_name_and_phone_number():
     print("Contact Information: ",name, phone)
     np= {'name': name, 'number': phone}
     return np
-    #print("Enter name and phone number...")
+
 
 def save_name_and_phone_number_in_the_storage(record):
     print("Saving records...")
     print(record["name"])
     base.append(record)
+    index=base.index(record)
+    print(index,record)
+
+def del_abon():
+    print(base)
+    ind = int(input('Enter number in the list '))
+    print (base[ind])
+    del base[ind]
+        
 def get_all_records():
     print("Get all records...")
+
 def display_records():
     print("Display records...")
+
 def main():
     print("MyPhonebook")
     start()
 
     print(base)
-    #rec = inpute_name_and_phone_number()
-    #save_name_and_phone_number_in_the_storage(rec)
-    #print(base)
-    #rec = inpute_name_and_phone_number()
-    #save_name_and_phone_number_in_the_storage(rec)
-    #print(base)
     get_all_records()
     display_records()
     print(rec)
 
 if __name__ == "__main__":
     main()
-#print (base)
+
 
 
 
